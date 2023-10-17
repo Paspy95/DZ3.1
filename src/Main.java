dcc11544852fcd555d49fa8ae5b852ff2e9124d2
Roman Gudov <gudov.p2@mail.ru>
2023-10-17 17:20:47 +0300

    change src/

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int priceTicket = 114_241;
-        int bonus = priceTicket / 20;
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
